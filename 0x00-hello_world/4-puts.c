4d857b7ac28fe67496b118c41f69162e62c25959
antoventures <antoventures@gmail.com>
2023-08-10 08:38:15 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  * using puts function
- * return 0(success)
+ * Return: 0(success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
