c6366ad5ef2e058e133a4b8b0ab74b8770d47cbd
antoventures <antoventures@gmail.com>
2023-08-11 09:03:01 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,9 +13,9 @@ int main(void)
 	char abz;
 	abz = 'a';
 
-	for(abz='a';abz<='z';abz++)
+	for(abz = 'a';abz <= 'z';abz++)
 		putchar(abz);
-	for(abz='A';abz<='Z';abz++)
+	for(abz = 'A';abz <= 'Z';abz++)
 		putchar(abz);
 	putchar('\n');
 
