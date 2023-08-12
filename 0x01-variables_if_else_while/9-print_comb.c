a157d72d43d32a86fe2e51bd40bf115b72878dff
antoventures <antoventures@gmail.com>
2023-08-12 01:55:08 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar(i);
+	if (i < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	return (0);
+}
