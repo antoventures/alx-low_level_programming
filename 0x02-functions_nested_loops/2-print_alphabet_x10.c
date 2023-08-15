8a95401fa2dd9b98cab264b21f2537a715090a09
antoventures <antoventures@gmail.com>
2023-08-15 07:33:48 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ * Return: void
+ */
+
+
+void print_alphabet_x10(void)
+{
+	int i;
+	i = 0;
+
+	while (i <= 10)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+	i++;
+	}
+
+}
