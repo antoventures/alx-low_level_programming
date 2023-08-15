0a95848ac1cb1abc80a4cc7ffa1c57eba0aede4d
antoventures <antoventures@gmail.com>
2023-08-15 07:40:34 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,21 +7,20 @@
 
 void print_alphabet_x10(void)
 {
-	int i;
+int i;
 
-	i = 0;
+i = 0;
 
-	while (i <= 10)
+while (i <= 10)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		char c;
-
-		for (c = 'a'; c <= 'z'; c++)
-		{
-			_putchar(c);
-			_putchar('\n');
-		}
-	
-	i++;
+		_putchar(c);
 	}
 
+_putchar('\n');
+i++;
+}
 }
