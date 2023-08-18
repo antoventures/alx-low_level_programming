051a57e6d3f8a196083e57060d6f187db4b5c73d
antoventures <antoventures@gmail.com>
2023-08-18 06:05:50 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ int main(void)
 
         i = 0;
 
-        while (i < 10)
-        {
-                putchar(i);//There is no incremental i so i will always remain at 0//
-        }
-
+/*        while (i < 10)
+ *      {
+ *              putchar(i);
+ *	}
+ */
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
